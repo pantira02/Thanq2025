51d4b519d7a4e36c4a5bcee13dc8759337bccf3d
pantira02 <maiiner2564@gmail.com>
2025-06-19 02:16:04 +0700

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,9 @@
+function filterByService(serviceName) {
+  alert("คุณเลือกบริการ: " + serviceName + " (ระบบกรองกำลังพัฒนา)");
+  // ในเวอร์ชันเต็มจะใช้ fetch() ดึงเฉพาะผู้รับงานตามบริการ
+}
+
+function chatWith(name) {
+  alert("กำลังเปิดแชทกับ " + name + " ผ่าน LINE OA");
+  // ใส่ลิงก์ LIFF หรือ LINE OA
+}
